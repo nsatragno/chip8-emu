366696e6e8f243c90905eaf53a1b93fb159b1c23
Nina Satragno <nsatragno@gmail.com>
2021-05-19 17:14:57 -0400

    More instructions, logging

--- chip8-emu/src/cpu.h
+++ chip8-emu/src/cpu.h
@@ -5,19 +5,34 @@
 #include "src/frame_buffer.h"
 #include "src/registers.h"
 
+// A CHIP-8 complete CPU.
 class Cpu {
 public:
-	static constexpr unsigned int kMaxMemory = 4069;
+	// The position of memory from which user program data can start.
+	static constexpr unsigned int kMinAddressableMemory = 0x200;
 
-	uint8_t peek(uint16_t address);
+	// The highest valid memory position.
+	static constexpr unsigned int kMaxMemory = 0xfff;
+
+	// The size of the program stack.
+	static constexpr unsigned int kStackSize = 32;
+
+	// Returns the contents of memory at |address|.
+	uint8_t peek(uint16_t address) const;
+
+	// Returns true if the machine was able to execute |instruction| successfully, false otherwise.
+	bool execute(uint16_t instruction);
+
+	uint16_t pc() { return pc_; }
 
 private:
-	Registers registers;
-	FrameBuffer buffer;
-	uint8_t stack[64] = { {0} };
-	uint8_t delay = 0;
-	uint8_t sound = 0;
-	uint16_t pc = 0;
+	Registers registers_;
+	FrameBuffer buffer_;
+	uint16_t stack_[kStackSize];
+	uint8_t sp_ = 0;
+	uint8_t delay_ = 0;
+	uint8_t sound_ = 0;
+	uint16_t pc_ = 0;
 
-	uint8_t memory[kMaxMemory] = { {0} };
+	uint8_t memory_[kMaxMemory + 1] = { {0} };
 };
---
+++ chip8-emu/src/logging.h
@@ -0,0 +1,11 @@
+#include <string>
+
+namespace logging {
+
+enum class Level {
+	INFO, WARN, ERROR
+};
+
+void log(Level level, const std::string& text);
+
+}  // namespace log
