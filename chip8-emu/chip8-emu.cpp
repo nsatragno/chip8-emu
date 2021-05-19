9d1b8fd1b69047548ebccbc09ec7093343edffa2
Nina Satragno <nsatragno@gmail.com>
2021-05-19 14:58:10 -0400

    Add vs template.

---
+++ chip8-emu/chip8-emu.cpp
@@ -0,0 +1,12 @@
+﻿// chip8-emu.cpp : Defines the entry point for the application.
+//
+
+#include "chip8-emu.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
